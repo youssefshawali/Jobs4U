e998fb709d84af1932bd3bcb231611ff23fc6ad3
ahmed ramadan <ar4112002@gmail.com>
2024-03-05 19:17:28 +0200

    apirespon

--- src/main/java/com/global/Controller/LoginController.java
+++ src/main/java/com/global/Controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.global.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.global.Entity.Login;
 import com.global.Services.LoginService;
 
+import ApiResponse.Response;
+
 @RestController
 @RequestMapping("/login")
 public class LoginController {
@@ -16,8 +20,29 @@ public class LoginController {
 	@Autowired
 	LoginService loginService;
 
+//	@PostMapping("/")
+//	public Object login(@RequestBody Login login) {
+//		return loginService.login(login.getEmail(),login.getPassword());
+//	}
+	
+	
 	@PostMapping("/")
-	public Object login(@RequestBody Login login) {
-		return loginService.login(login.getEmail(),login.getPassword());
+	public ResponseEntity<Response<Object>> login(@RequestBody Login login) {
+	    Object result = loginService.login(login.getEmail(), login.getPassword());
+	    Response<Object> response = new Response<>();
+
+	    if (result != null) {
+	        // Login successful
+	        response.setStatus(HttpStatus.OK.value());
+	        response.setMessage("Success");
+	        response.setData(result);
+	    } else {
+	        // Login failed
+	        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+	        response.setMessage("Login failed");
+	        response.setData(null);
+	    }
+
+	    return ResponseEntity.ok(response);
 	}
 }
